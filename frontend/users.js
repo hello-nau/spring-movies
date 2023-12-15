5ee22ac193e0d8f78365e5577b5fb9da1303c2ee
Yana T <yana.work.related@gmail.com>
2023-12-14 19:07:57 -0800

    js delete users

--- frontend/users.js
+++ frontend/users.js
@@ -1,26 +1,31 @@
 const userList = document.querySelector("#user-list");
 
+
 window.onload = async function(evt) {
   evt.preventDefault();
-  console.log("Getting users");
-  axios.get("http://localhost:8080/users").then((res) => {
-    console.log(res.data);
-    populateUsers(res.data);
-  })
+  // console.log("Getting users");
+  // axios.get("http://localhost:8080/users").then((res) => {
+  //   console.log(res.data);
+  //   populateUsers(res.data);
+  // })
+  fetchUsers();
 } 
+
+
 function populateUsers(userData) {
   let usrLi = document.getElementById("usr-li");
 
   for (let user of userData) {
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(user.userName));
+    li.onclick = function() {
+      deleteUser(user.userName);
+    };
     usrLi.appendChild(li);
-    // li.appendChild(document.createTextNode(user.movieSet));
-    // usrLi.appendChild(li);
-
   }
   userList.appendChild(usrLi);
 }
+
 
 function createUser() {
   let userNameInput = document.getElementById("username");
@@ -30,7 +35,6 @@ function createUser() {
     alert("Please enter a valid user name");
     return;
   }
-
   axios.post("http://localhost:8080/users", {userName: newUserName})
   .then((res) => {
     console.log("User created successfully: ", res.data);
@@ -41,6 +45,31 @@ function createUser() {
     console.error("Error creating user: ", error);
   });
 }
-function deleteUser() {
-  
+
+
+function deleteUser(userName) {
+if (confirm("Are you sure you want to delete this user?")) {
+  axios.delete(`http://localhost:8080/users/${userName}`)
+  .then((res) => {
+    console.log("User deleted: ", res.data);
+    fetchUsers();
+  })
+  .catch((error) => {
+    console.error("Error deleting a user: ", error);
+  });
+}
+}
+
+
+function openUser(userName) {}
+function addMovieToUser(userName) {}
+
+function fetchUsers() {
+  console.log("Getting users...");
+  axios.get("http://localhost:8080/users").then((res) => {
+    console.log(res.data);
+    let usrLi = document.getElementById("usr-li");
+    usrLi.innerHTML = '';
+    populateUsers(res.data);
+  })
 }
