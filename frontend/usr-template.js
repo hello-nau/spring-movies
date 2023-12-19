266ea83135741b9acbd28d4f132fb7e84374d76b
Yana T <yana.work.related@gmail.com>
2023-12-18 17:46:49 -0800

    added openMovie

--- frontend/usr-template.js
+++ frontend/usr-template.js
@@ -13,12 +13,53 @@ window.onload = async function() {
   let usrMvLi = document.getElementById("movie-list");
   for (let movie of movieList) {
     let li = document.createElement("li");
-    li.appendChild(document.createTextNode(movie.id));
+      let span = document.createElement("span");
+    span.textContent = movie.id;
 
+
+    span.addEventListener("click", function() {
+      openMovie(userData.userName, movie.id);
+    });
+
+
+    li.appendChild(span);
+    li.appendChild(document.createTextNode(": "));
     li.appendChild(document.createTextNode(movie.description));
+
+    
 
     usrMvLi.appendChild(li);
   }
-  
+
   }
+
+function openMovie(userName, id) {
+
+console.log("openMovie is called");
+const userTemplateUrl = "/movie.html";
+
+fetch(userTemplateUrl) 
+  .then(response => response.text())
+  .then(templateHtml => {
+         axios.get(`http://localhost:8080/users/${userName}/movieList/${id}`)
+  .then(response => {
+    let userData = response.data;
+
+    localStorage.setItem('userData', JSON.stringify(userData));
+
+    let userContent = Mustache.render(templateHtml, userData);
+    let div = document.createElement("div");
+    div.classList.add("usr-container");
+
+    div.innerHTML = userContent;
+
+     window.open(`/movie.html`, '_blank');
+  })
+
+  }) 
+}
+
+
+
+
 
