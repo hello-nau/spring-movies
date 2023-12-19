cf63f3981538d3ad8e27cf86718da5823dc32013
Yana T <yana.work.related@gmail.com>
2023-12-18 17:05:44 -0800

    front username load movies

--- frontend/usr-template.js
+++ frontend/usr-template.js
@@ -3,17 +3,19 @@
 window.onload = async function() {
 
   let userData = JSON.parse(localStorage.getItem('userData'));
-  let movieString = userData.movieSet;
-  let movieList = JSON.parse(movieString);
+  // let movieString = userData.movieSet;
+  let name = document.getElementById("userNm");
+  name.innerHTML = userData.userName;
+  let movieList = userData.movies;
 
   console.log(movieList);
 
   let usrMvLi = document.getElementById("movie-list");
   for (let movie of movieList) {
     let li = document.createElement("li");
-    li.appendChild(document.createTextNode(movieList.id));
+    li.appendChild(document.createTextNode(movie.id));
 
-    li.appendChild(document.createTextNode(movieList.description));
+    li.appendChild(document.createTextNode(movie.description));
 
     usrMvLi.appendChild(li);
   }
